2c081843cadca2f9fe513fc12b26b4f2105c0258
KadenHD <71968563+KadenHD@users.noreply.github.com>
2026-02-16 18:37:49 +0100

    cosmetics

--- rust-chatroom-websocket/src/lib.rs
+++ rust-chatroom-websocket/src/lib.rs
@@ -9,7 +9,7 @@ pub fn run() {
     let cfg = config::config();
     logging::init(&cfg.log_level, None);
 
-    log::info!("Logging in {} level", cfg.log_level);
-    log::info!("Running app '{}' in {} mode", cfg.app_name, cfg.env_name);
-    log::info!("Listening on {}:{}", cfg.app_host, cfg.app_port);
+    log::info!("Logging in '{}' level", cfg.log_level);
+    log::info!("Running app '{}' in '{}' mode", cfg.app_name, cfg.env_name);
+    log::info!("Listening on '{}:{}'", cfg.app_host, cfg.app_port);
 }
